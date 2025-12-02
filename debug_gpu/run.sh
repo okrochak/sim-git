9b4fcb1d8fbd9d6e616ae57982f8d3c418ecb5ff
Alex Krochak <o.krochak@fz-juelich.de>
2025-12-02 15:59:48 +0100

    increase write frequency

--- debug_gpu/run.sh
+++ debug_gpu/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 #SBATCH --account=jhpc54
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --job-name=cyclic-simulation
 #SBATCH --ntasks-per-node=4
 #SBATCH --time=01:00:00
-#SBATCH --partition=dc-gpu
+#SBATCH --partition=dc-gpu-devel
 #SBATCH --output=slurm-out.%j
 #SBATCH --error=slurm-err.%j
 #SBATCH --gres=gpu:4
 
-source evn_maia
+source env_maia_gpu
 
 mclean
 srun maia properties_run.toml 
