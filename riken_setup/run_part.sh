0f97ba9e150d53b8f3c4dd6ba35aae056587fd8e
Alex Krochak <o.krochak@fz-juelich.de>
2026-04-23 10:17:41 +0200

    add riken setup dir

--- riken_setup/run_part.sh
+++ riken_setup/run_part.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #SBATCH --account=jhpc54
-#SBATCH --nodes=4
-#SBATCH --job-name=riken_SS_noparts
+#SBATCH --nodes=6
+#SBATCH --job-name=riken_SS_wparts
 #SBATCH --ntasks-per-node=128
 #SBATCH --time=06:00:00
 #SBATCH --partition=dc-cpu
@@ -11,4 +11,4 @@
 source env_maia
 
 mclean
-srun maia_nasal properties_run.toml 
+srun maia_nasal properties_particles.toml 
